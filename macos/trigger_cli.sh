a4a3d13a9cd1c336070eae755dc2874b2fbebb7e
Alex Brooke <akb.sbc@gmail.com>
2018-05-30 20:35:51 -0500

    Update trigger_cli.sh

--- macos/trigger_cli.sh
+++ macos/trigger_cli.sh
@@ -3,6 +3,8 @@ BASE_URL=https://circleci.com/api/v1.1/project/github/purduesigbots/pros-cli
 echo =============== TRIGGER CLI BUILD ===============
 # will run build and return build number, which we need to construct the endpoint for blocking
 BUILD_NUMBER=$(curl -X POST $BASE_URL?circle-token=$CLI_TOKEN | jq --raw-output --exit-status '.build_number)
+# save to a file for access in another script later
+echo $BUILD_NUMBER > CLI_BUILD_NUM
 
 echo =============== WAITING FOR BUILD TO COMPLETE ===============
 ./macos/wait_for_build.sh $CLI_TOKEN $BASE_URL/$BUILD_NUMBER 3600
