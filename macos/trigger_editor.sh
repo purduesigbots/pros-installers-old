9bf1530ee5f1734c975e648b7001aa8f2d4a95ab
Alex Brooke <akb.sbc@gmail.com>
2018-05-30 20:36:37 -0500

    Update trigger_editor.sh

--- macos/trigger_editor.sh
+++ macos/trigger_editor.sh
@@ -3,5 +3,8 @@ BASE_URL=https://circleci.com/api/v1.1/project/github/purduesigbots/atom
 echo =============== TRIGGER EDITOR BUILD ===============
 # will run build and return build number, which we need to construct the endpoint for blocking
 BUILD_NUMBER=$(curl -X POST $BASE_URL?circle-token=$CLI_TOKEN | jq --raw-output --exit-status '.build_number)
+# save build number to a file for access in another script later
+echo $BUILD_NUMBER > EDITOR_BUILD_NUMBER
+
 echo =============== WAITING FOR BUILD TO COMPLETE ===============
 ./macos/wait_for_build.sh $CLI_TOKEN $BASE_URL/$BUILD_NUMBER 3600
