e1c2463ad0568a1f4de1c0b0fdcd96a26d2395ee
Alex Brooke <akb.sbc@gmail.com>
2018-05-31 20:36:45 -0500

    Update trigger_cli.sh

--- macos/trigger_cli.sh
+++ macos/trigger_cli.sh
@@ -2,9 +2,19 @@
 BASE_URL=https://circleci.com/api/v1.1/project/github/purduesigbots/pros-cli
 echo =============== TRIGGER CLI BUILD ===============
 # will run build and return build number, which we need to construct the endpoint for blocking
-BUILD_NUMBER=$(curl -X POST $BASE_URL?circle-token=$CLI_TOKEN | jq --raw-output --exit-status '.build_number)
+BUILD_NUMBER=$(curl -X POST $BASE_URL?circle-token=$CLI_TOKEN | jq --raw-output --exit-status '.build_number')
 # save to a file for access in another script later
 echo $BUILD_NUMBER > CLI_BUILD_NUM
 
 echo =============== WAITING FOR BUILD TO COMPLETE ===============
 ./macos/wait_for_build.sh $CLI_TOKEN $BASE_URL/$BUILD_NUMBER 3600
+
+echo =============== BUILD COMPLETE ===============
+BUILD_STATUS=$(curl $BASE_URL/$BUILD_NUMBER?circle-token=$CLI_TOKEN | jq --raw-output --exit-status '.outcome')
+if [[ $BUILD_STATUS != 'success' ]] then
+  echo "Building CLI failed! Check build $BUILD_NUMBER for more details"
+  exit 1
+fi
+# success- get artifact
+CLI_ARTIFACT_BASE_URL=$(curl $BASE_URL/$BUILD_NUMBER/artifacts?circle-token=$CLI_TOKEN | jq --raw-output --exit-status '.[0].url')
+curl -o pros-cli.pkg $CLI_ARTIFACT_BASE_URL?circle-token=$CLI_TOKEN
