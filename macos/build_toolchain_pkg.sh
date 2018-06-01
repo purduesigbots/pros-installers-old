68bb653a6484631dcc86dfec916faa709498f279
Alex Brooke <akb.sbc@gmail.com>
2018-06-01 13:06:00 -0500

    Update build_toolchain_pkg.sh

--- macos/build_toolchain_pkg.sh
+++ macos/build_toolchain_pkg.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+mkdir -p /tmp/artifacts
+
 pkgbuild --root macos/toolchainpkg/ROOT \
 	--scripts macos/toolchainpkg/scripts \
 	--version 2017-q4-major \
 	--identifier edu.purdue.cs.pros.pros-toolchain \
 	--install-location /usr/local/lib \
-	pros-toolchain.pkg
+	/tmp/artifacts/pros-toolchain.pkg
