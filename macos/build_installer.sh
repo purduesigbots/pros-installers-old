e22f4d8b8d782d62a7b20d8229133e8c20311a59
Alex Brooke <akb.sbc@gmail.com>
2018-06-02 13:55:35 -0500

    update distribution

--- macos/build_installer.sh
+++ macos/build_installer.sh
@@ -5,4 +5,5 @@ ls .
 ls macos/prospkg
 pwd
 
-productbuild --resources macos/prospkg/resources --scripts macos/prospkg/scripts --distribution macos/prospkg/distribution.xml --package-path /tmp/artifacts --sign $APPLE_CCN pros-macOS.pkg
+cp /tmp/artifacts/{pros-editor.pkg,pros-cli.pkg,pros-toolchain.pkg} macos/prospkg
+productbuild --resources macos/prospkg/resources --scripts macos/prospkg/scripts --distribution macos/prospkg/distribution.xml --package-path macos/prospkg --sign $APPLE_CCN pros-macOS.pkg
