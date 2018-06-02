bf939613acc4a8289c51581fa9028c9eebea18b5
Alex Brooke <akb.sbc@gmail.com>
2018-06-01 23:30:25 -0500

    fhfsdkjfdskhjfs

--- macos/build_installer.sh
+++ macos/build_installer.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-
-productbuild --resource macos/prospkg/resources \
+echo debug: $APPLE_CCN
+productbuld --resource macos/prospkg/resources \
   --package-path /tmp/artifacts/proscli.pkg \
   --package-path /tmp/artifacts/pros-toolchain.pkg \
   --package-path /tmp/artifacts/pros-editor.pkg \
   --distribution macos/prospkg/distribution.xml \
+  --sign $APPLE_CCN \
   pros-macOS.unsigned.pkg
-
-productsign --sign $APPLE_CCN pros-macOS.unsigned.pkg pros-macOS.pkg
