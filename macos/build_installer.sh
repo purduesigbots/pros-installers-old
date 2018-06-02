bc81a149bab5782cf0c6e1f1e349f3f45004727c
Alex Brooke <akb.sbc@gmail.com>
2018-06-02 14:32:53 -0500

    asfdfdgfhgfjfsgrvfdgr

--- macos/build_installer.sh
+++ macos/build_installer.sh
@@ -5,5 +5,5 @@ ls .
 ls macos/prospkg
 pwd
 
-cp /tmp/artifacts/{pros-editor.pkg,pros-cli.pkg,pros-toolchain.pkg} macos/prospkg
-productbuild --resources macos/prospkg/resources --scripts macos/prospkg/scripts --distribution macos/prospkg/distribution.xml --package-path macos/prospkg --sign $APPLE_CCN pros-macOS.pkg
+# cp /tmp/artifacts/{pros-editor.pkg,pros-cli.pkg,pros-toolchain.pkg} macos/prospkg
+productbuild --distribution macos/prospkg/distribution.xml --resources macos/prospkg/resources --scripts macos/prospkg/scripts --package-path /tmp/artifacts --sign $APPLE_CCN pros-macOS.pkg
