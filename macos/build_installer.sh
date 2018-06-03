4cab35dcec8885b89d9b243c0f82485264338052
Alex Brooke <akb.sbc@gmail.com>
2018-06-03 12:16:48 -0500

    why doesn't this work wth

--- macos/build_installer.sh
+++ macos/build_installer.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
-ls /tmp/artifacts
-ls .
-ls macos/prospkg
-pwd
-
-# cp /tmp/artifacts/{pros-editor.pkg,pros-cli.pkg,pros-toolchain.pkg} macos/prospkg
+echo $APPLE_CCN
 productbuild --distribution macos/prospkg/distribution.xml --resources macos/prospkg/resources --scripts macos/prospkg/scripts --package-path /tmp/artifacts --sign $APPLE_CCN pros-macOS.pkg
