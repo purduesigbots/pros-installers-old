8209c77068dd187104a1997325bc56fa981de16a
Alex Brooke <akb.sbc@gmail.com>
2018-06-03 12:31:15 -0500

    cautious optimism

--- macos/build_installer.sh
+++ macos/build_installer.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-echo $APPLE_CCN
-productbuild --distribution macos/prospkg/distribution.xml --resources macos/prospkg/resources --scripts macos/prospkg/scripts --package-path /tmp/artifacts --sign $APPLE_CCN pros-macOS.pkg
+productbuild --distribution macos/prospkg/distribution.xml --resources macos/prospkg/resources --scripts macos/prospkg/scripts --package-path /tmp/artifacts --sign "$APPLE_CCN" pros-macOS.pkg
