8e09670172ac8a67ee762d37030a4f1c8976cac2
Alex Brooke <akb.sbc@gmail.com>
2018-06-08 14:34:41 -0500

    add signing again
    - apparently circle is going to force us to use fastlane. why!!! i dont want it

--- macos/build_installer.sh
+++ macos/build_installer.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 productbuild --distribution macos/prospkg/distribution.xml --resources macos/prospkg/resources --scripts macos/prospkg/scripts --package-path /tmp/artifacts pros-macOS.unsigned.pkg
+productsign --sign "$APPLE_CCN" pros-macOS.unsigned.pkg pros-macOS.pkg
